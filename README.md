d3ff2f27705a02a603da00027ab48582eefc46cc
Lauren Spatz <laurens914@gmail.com>
2015-12-03 11:50:21 -0800

    add about me, update game and index files

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # guessing-game
+Lauren Spatz
 Guessing game for 201 class
+Multiple questions and a Joke for the first week project of 201
+
+Joke!
+What program did the Jedi use to open a PDF file?
+
+
+
+
+
+
+
+
+
+
+
+
+Adobe Wan Kenobi, haha
--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 var user = prompt('What is your name?');
 console.log("users name=" + user);
 alert('Happy weekend! ' + user + ', time for a cold beer!');
-
+//
 var correct = 0;
 var a = prompt('Do I have any siblings? (yes or no repsonses)');
 console.log("first prompt finished");
@@ -15,7 +15,7 @@ if (a === 'yes' || a === 'y' || a === 'Yes' || a === 'Y'){
   console.log("frist response incorrect")
   alert('incorrect, sorry ' + user + ', I have 1 sister');
 }
-
+//
 var b = prompt('Have I lived anywhere but Seattle?');
 if (b === 'yes' ||  b ==='y' || b === 'Yes' || b === 'Y'){
   console.log("second response: " + b);
@@ -26,7 +26,7 @@ if (b === 'yes' ||  b ==='y' || b === 'Yes' || b === 'Y'){
   console.log("second response incorrect");
   alert('Sorry ' + user + ', I lived in Pullman for 2 years');
 }
-
+//
 var c = prompt('Have I ever been to Austrailia?');
 if (c === 'no' || c === 'n' || c === 'No' || c ==='N') {
   console.log("third response: " + c);
@@ -46,9 +46,45 @@ console.log(typeof(ageToNumber));
 if (ageToNumber === 25){
   alert('Correct ' + user + '!');
   correct = correct + 1;
-} else if (ageToNumber >= 26){
-  alert('Too High!');
+} else if ( ageToNumber >= 26){
+   alert('Too Old!');
 } else {
-  alert('Too Low');
+  alert('Too Young');
 }
+
+var number = 44;
+  var userNumber = parseInt(prompt("what number am I thinking? (1-50)"));
+  if (userNumber === number){
+    alert('correct!');
+    correct = correct + 1;
+  }
+while (userNumber !== number) {
+  if (userNumber > number){
+  userNumber = parseInt(prompt('too High'));
+} else {
+   userNumber = parseInt(prompt('too low'));
+  }
+ }
+ if (userNumber === number){
+   alert('correct!');
+   correct = correct + 1;
+ }
+
+function quarterbackGame(){
+ var multipleChoice = ['russell wilson','carson palmer','blaine gabbert','nick foles'];
+ var multipleChoiceQuestion = prompt('Name a Quarterback in the NFC West').toLowerCase();
+ for (var i = 0; i< multipleChoice.length; i++){
+   console.log(i)
+   if (multipleChoiceQuestion === multipleChoice[i]){
+     alert('correct');
+     console.log('Yes!!!!');
+     correct = correct + 1;
+     return;
+  }
+}
+alert('Wrong');
+}
+quarterbackGame();
+
+//
 alert('you got ' + correct + ' right answers!');
