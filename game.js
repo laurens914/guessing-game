35f6da7153316967d6a51d6fbf87a259d628031c
Lauren Spatz <laurens914@gmail.com>
2015-12-04 10:09:21 -0800

    add CSS and updated html and js with functions

--- game.js
+++ game.js
@@ -4,58 +4,86 @@ console.log("users name=" + user);
 alert('Happy weekend! ' + user + ', time for a cold beer!');
 //
 var correct = 0;
+var sister = document.getElementById('sister');
+function siblings(){
 var a = prompt('Do I have any siblings? (yes or no repsonses)');
 console.log("first prompt finished");
 if (a === 'yes' || a === 'y' || a === 'Yes' || a === 'Y'){
   console.log("first response: " + a);
-  alert('Yes! Correct ' + user + ' I have 1 sister');
+  sister.className = 'right';
+  sister.textContent='Yes! Correct ' + user + ' I have 1 sister';
   correct = correct + 1;
   console.log("correct = " + correct);
 } else {
   console.log("frist response incorrect")
-  alert('incorrect, sorry ' + user + ', I have 1 sister');
+  sister.className='wrong'
+  sister.textContent= 'Incorrect, sorry ' + user + ', I have 1 sister';
 }
+}
+siblings();
 //
+var home = document.getElementById('home');
+function living() {
 var b = prompt('Have I lived anywhere but Seattle?');
 if (b === 'yes' ||  b ==='y' || b === 'Yes' || b === 'Y'){
   console.log("second response: " + b);
-  alert('Correct ' + user + '! I lived in Pullman during college');
+  home.className='right';
+  home.textContent ='Correct ' + user + '! I lived in Pullman during college';
   correct = correct + 1;
   console.log("correct =" + correct);
 } else {
   console.log("second response incorrect");
-  alert('Sorry ' + user + ', I lived in Pullman for 2 years');
+  home.className='wrong';
+  home.textContent='Sorry ' + user + ', I lived in Pullman for 2 years';
 }
+}
+living();
 //
+var aussie = document.getElementById('aussie');
+function austrailia(){
 var c = prompt('Have I ever been to Austrailia?');
 if (c === 'no' || c === 'n' || c === 'No' || c ==='N') {
   console.log("third response: " + c);
-  alert('Correct ' + user + '! But I would love to go!');
+  aussie.className='right';
+  aussie.textContent='Correct ' + user + '! But I would love to go!';
   correct = correct + 1;
   console.log("correct =" + correct);
 } else {
   console.log("third response incorrect");
-  alert('Sorry, ' + user + ', sadly, no I have not been there.');
+  aussie.className='wrong';
+  aussie.textContent='Sorry, ' + user + ', sadly, no I have not been there.';
 }
+}
+austrailia();
 
+var old = document.getElementById('old');
+function birthday() {
 var age = prompt('How Old am I?');
 console.log("age is a...: " + typeof(age));
 var ageToNumber = parseInt(age);
 console.log("ageToNumber =" + ageToNumber);
 console.log(typeof(ageToNumber));
 if (ageToNumber === 25){
-  alert('Correct ' + user + '!');
+  old.className='right';
+  old.textContent='Correct ' + user + '!';
   correct = correct + 1;
 } else if ( ageToNumber >= 26){
-   alert('Too Old!');
+  old.className='wrong';
+  old.textContent='Too Old! I am only 25';
 } else {
-  alert('Too Young');
+  old.className='wrong';
+  old.textContent='Too Young, I am actully 25';
 }
+}
+birthday();
 
+var random = document.getElementById('random');
+function number (){
 var number = 44;
   var userNumber = parseInt(prompt("what number am I thinking? (1-50)"));
   if (userNumber === number){
-    alert('correct!');
+    random.className='right';
+    random.textContent= 'Woah! correct!';
     correct = correct + 1;
   }
 while (userNumber !== number) {
@@ -66,25 +94,32 @@ while (userNumber !== number) {
   }
  }
  if (userNumber === number){
-   alert('correct!');
+   random.className='right';
+   random.textContent= 'Great job! Correct!';
    correct = correct + 1;
  }
+ }
+ number();
 
+var qb = document.getElementById('qb');
 function quarterbackGame(){
  var multipleChoice = ['russell wilson','carson palmer','blaine gabbert','nick foles'];
  var multipleChoiceQuestion = prompt('Name a Quarterback in the NFC West').toLowerCase();
  for (var i = 0; i< multipleChoice.length; i++){
    console.log(i)
    if (multipleChoiceQuestion === multipleChoice[i]){
-     alert('correct');
+     qb.className='right';
+     qb.textContent='Correct, Russell Wilson, Carson Palmer, Blaine Gabbert, and Nick Foles are the NFC West QBs';
      console.log('Yes!!!!');
      correct = correct + 1;
      return;
   }
 }
-alert('Wrong');
+qb.className='wrong'
+qb.textContent='Wrong, Russell Wilson, Carson Palmer, Blaine Gabbert, and Nick Foles are the NFC West QBs';
 }
 quarterbackGame();
 
 //
-alert('you got ' + correct + ' right answers!');
+var goodjob = document.getElementById('goodjob')
+goodjob.textContent='You got ' + correct + ' right answers!';
